7c1a314a0be3cb11924b898752eee98a286436df
athif05 <athif.hussain05@gmail.com>
2026-09-02 13:11:45 +0530

    rg-github-pipeline

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "subscription_id" {
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
   type        = string
-  default     = "rg-github-pipeline-testing-cicd"
+  default     = "rg-github-pipeline"
 }
 
 variable "location" {
